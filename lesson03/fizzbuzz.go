41e2946809d1579260003e4b5f5e2398abcaf35c
k.sirazheev <k.sirazheev@gmail.com>
2020-12-08 09:46:41 +0300

    fix 'else / if' statement

--- lesson03/fizzbuzz.go
+++ lesson03/fizzbuzz.go
@@ -5,22 +5,14 @@ import "fmt"
 func main() {
 	for i := 1; i <= 100; i++ {
 
-		if i%3 == 0 {
-			fmt.Println("Fizz")
-		}
-		if i%5 == 0 {
-			fmt.Println("Buzz")
-		}
-
-		if i%3 == 0 && i%5 == 0 {
+		if i%15 == 0 {
 			fmt.Println("FizzBuzz")
-		}
-
-		if i%3 != 0 && i%5 != 0 {
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else {
 			fmt.Println(i)
 		}
-
-		fmt.Println(i)
-
 	}
 }
