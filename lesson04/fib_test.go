103c34d49cb2a2093d14f7bb91e06f105c9132f9
k.sirazheev <k.sirazheev@gmail.com>
2021-01-09 12:00:05 +0300

    add example for fibonacci func

--- lesson04/fib_test.go
+++ lesson04/fib_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 var result int
 func BenchmarkFib(b *testing.B) {
@@ -8,3 +11,11 @@ func BenchmarkFib(b *testing.B) {
 	for i:= 0; i < b.N; i++ {
 		res = Fib(20) }
 	result = res }
+
+
+func ExampleFib() {
+	n := 5
+	fmt.Println(Fib(n))
+	// Output: All line: map[0:1 1:1 2:1 3:2 4:3 5:5]
+	//5
+}
